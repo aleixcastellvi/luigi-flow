38270fcb850beee21b1f1c281b32ad6b3aa511ad
aleixcastellvi <aleixcastellvip@gmail.com>
2024-03-14 00:00:21 +0100

    Improvements

--- api_data_fetcher_task.py
+++ api_data_fetcher_task.py
@@ -3,6 +3,7 @@
 import config
 import luigi
 import json
+import pandas as pd
 from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
@@ -31,14 +32,43 @@ class APIDataFetcherTask(luigi.Task):
 
         try:
             response = session.get(url, params=parameters)
-            data = json.loads(response.text)
-            print(data)
-        
+
+            raw_data = json.loads(response.text)
+
+            df = pd.json_normalize(raw_data['data'])
+
+            selected_df = df[['cmc_rank', 'name', 'symbol', 'quote.USD.price', 'date_added']]
+
+            renamed_df = selected_df.rename(columns = {'cmc_rank': 'Ranking', 
+                                                       'name':'Coin', 
+                                                       'symbol': 'Crypto ID', 
+                                                       'quote.USD.price':'Price', 
+                                                       'date_added':'Released'})
+
+            renamed_df['Price'] = renamed_df['Price'].apply(lambda x: self.formatted_value(x))
+
+            print(renamed_df)
+
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             print(e)
 
     def output(self):
         pass
+
+    def formatted_value(self, value):
+        split_price = str(value).split(".")[1]
+        if int(value) == 0 and split_price[:2] == "00":
+            return str(round(value, 4)).replace(".", ",")
+        else:
+            return self.thousands_values(value)
+
+    def thousands_values(self, value):
+        formated_val = "{:,.2f}".format(value)
+        integer, decimals = formated_val.split(".")
+        integer = integer.replace(",", ".")
+
+        return f"{integer},{decimals}"
+
 
 if __name__ == '__main__':
     luigi.run()
